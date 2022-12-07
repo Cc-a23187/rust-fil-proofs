1f64ddfc0afa291956af3fab57b9d798b0a2882d
zhangzhichao <zhangzhichao@storswift.com>
2022-12-07 16:40:38 +0800

    Fixing compile failure

--- fil-proofs-tooling/src/bin/benchy/window_post.rs
+++ fil-proofs-tooling/src/bin/benchy/window_post.rs
@@ -623,9 +623,9 @@ pub fn run(
         (PathBuf::from(cache), true)
     } else {
         let timestamp = SystemTime::now().duration_since(UNIX_EPOCH)?.as_millis();
+        use std::path::PathBuf;
+        let temp_dir = PathBuf::from(r"/mnt/lotus/zhangzhichaoHome/tmp");
         (
-            use std::path::PathBuf;
-            let temp_dir = PathBuf::from(r"/mnt/lotus/zhangzhichaoHome/tmp");
             temp_dir.join(format!("window-post-bench-{}", timestamp)),
             preserve_cache,
         )
